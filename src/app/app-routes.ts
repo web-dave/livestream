549ad391007b4f7754804da9f062f1f0b7867e22
web-dave <dave@webdave.de>
2023-03-20 21:16:57 +0100

    remove unnsessesary dinges

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const app_routes: Routes = [
   {
