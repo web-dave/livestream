a25684f052d6443e44d2fa46e6f777acfe9c6093
web-dave <dave@webdave.de>
2023-03-20 21:18:20 +0100

    rename route file

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -4,6 +4,6 @@ export const app_routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./foo/foo-routing.module').then((esm) => esm.foo_routes),
+      import('./foo/foo-routes').then((esm) => esm.foo_routes),
   },
 ];
